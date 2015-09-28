8b284a0ba50b18d15018e0b134f1e189d85d954c
zoojar <davidnewton@zoojar.com>
2015-09-28 17:48:32 +0100

    string fixes for version comp.

--- manifests/resources/repo/epel.pp
+++ manifests/resources/repo/epel.pp
@@ -1,7 +1,7 @@
 class openstack::resources::repo::epel {
   if ($::osfamily == 'RedHat' and
       $::operatingsystem != 'Fedora' and
-      versioncmp($::operatingsystemmajrelease, 6) >= 0 ) {
+      versioncmp("{$::operatingsystemmajrelease}", '6') >= 0 ) {
     include openstack::resources::repo::yum_refresh
 
     include ::epel
